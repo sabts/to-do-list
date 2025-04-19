0632b86519efe84bd2030a34934024ad352de3aa
sabts <sabrinataraschi@gmail.com>
2025-04-19 22:11:24 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,25 +10,30 @@ const tasks = [
       name: 'llorar por el código',
       completed: false
     }
-  ];
+];
 
-  //let allTask = [];
+//let allTask = [];
 
-  const countItemsLeft = () => {  
-    if(taskElement.childElementCount === 0) { 
-      itemsLeftElement.textContent = `No item left`} 
-    else {
-      itemsLeftElement.textContent =`item left ${taskElement.childElementCount}`;
-    }
-    console.log(itemsLeftElement.textContent)
+
+const countItemsLeft = () => {  
+  if(taskElement.childElementCount === 0) { 
+    itemsLeftElement.textContent = `No item left`}
+  else{
+     itemsLeftElement.textContent = `${taskElement.childElementCount} items left`
+  }
+//cuando use el filtro para que me diera el numero de task completadas pero sobre escribia el mensaje de no items left pero ahora cuando marco todas las tareas como completas me dice [Object Object] items left
+
+
+  //me suma el item left pero no me lo resta si elimino una
+  //seguro se sobreescribio con la funcion de (complete task) pero ya no se como solucionarlo, intentare darle otra vuelta más tarde pero si para el lunes sigue igual preguntare...
   }
 
 
-  const insertTasks = () =>{
-    taskElement.textContent = "";  //el padre
+const insertTasks = () =>{
+  taskElement.textContent = "";  //el padre (el taskElement)
 
     tasks.forEach(task => {
-      //El div --- el hijo 0
+      //El div --- el hijo 0 del task element
       const taskContainer = document.createElement('div');
       taskContainer.classList.add('task-container');
       //taskContainer.dataset.id = task.id;
@@ -43,12 +48,10 @@ const tasks = [
     
       //Para el label --- el hijo del div 1
       const labelCheckboxElement = document.createElement('label');
-      labelCheckboxElement.htmlFor = task.id
-     //Lo deje como reflexión: intente hacer algo con el data pero no me funcionó, el objeto se crea en la web pero no me deja marcar el check, asi que investigue "como aplicar el 'for' a un label en javaScript" :)
+      labelCheckboxElement.htmlFor = task.id;
      //sintaxis: Element.setAttribute(name, value); el name: seria el nombre del atributo que le agrego y el value en este caso, de quien es el for -- https://developer.mozilla.org/es/docs/Web/API/Element/setAttribute
       labelCheckboxElement.classList.add('task-text');
       labelCheckboxElement.textContent = task.name
-      //if (task.name === "") return; //si no le agrego esto me crea un dos cosas a la lista: el primer objeto vacio y el nuevo que cree Esto era cuando vacie el name de objeto
     
       //Eliminar task --- el hijo del div 2
       const deleteTaskElement = document.createElement('img');
@@ -59,26 +62,46 @@ const tasks = [
       taskContainer.append(checkboxElement, labelCheckboxElement, deleteTaskElement);
       taskElement.append(taskContainer)
     })
-    countItemsLeft()
   }
 
-  const completeTask = (id) => {
-    if(taskElement.children[0].children[0].checked === true){
-      console.log('tarea ' + id + ' está completada')
+const completeTask = (id) => {
+  for(let i = 0; i < tasks.length; i++){
+    if(tasks[i].id === id){
+      tasks[i].completed = !tasks[i].completed; 
+      //cambio el estado de true o false del task marcado 
+      // tasks[i].completed es false (no completada) 
+      // !tasks[i].completed es true (completada)
+      break
     }
-      //itemsLeftElement.textContent = 'All task complete'
+    const incompleteTaskList = tasks.filter(task => task.completed); // me devuelve lista de las tareas que no tiene el checked
+      
+    if(incompleteTaskList.length === 0){ // este if esta de adorno, no me lee cuando todas las tareas estan completas
+    //pero la lógica seria: sí el largo del array es 0, las tareas estan en true (completadas)
+    itemsLeftElement.textContent = "All task completed" 
+    } else{
+    itemsLeftElement.textContent = `item left ${incompleteTaskList.length}`
+    }
+    console.log(itemsLeftElement.textContent)
+    
   }
+  if(taskElement.children[0].children[0].checked === true){
+  console.log('tarea ' + id + ' está completada') // al menos puedo comprobar aqui que las tareas si se estan marcando como completada
+  }  
+  insertTasks();   
+  countItemsLeft();
+}
 
 //no se editan las tareas en el DOM ni se eliminan del DOM, completar una tarea implica cambiar el completed de true a false o de false a true y eliminarla implica eliminarla del array porque cada vez que algo cambie volverás a pintar todo el array entero con sus cambios en el DOM
 const deleteTask = (id) => {
   for (let i = 0; i < tasks.length; i++) {
     if (tasks[i].id === id) {
       console.log('estas borrando ' + + tasks[i].id)
-      tasks.splice(i, 1); // Eliminar la tarea del array
-      break;   
+      tasks.splice(i, 1); // Eliminar la tarea del tasks
+      break; 
     }
     }
 insertTasks();
+countItemsLeft()
 }
 
 const createTask = (event) => {
